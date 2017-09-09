94d0d7462d836ba5796e34dff77a49e08005fd50
Maximilian Stroh <Hisako1337@gmail.com>
2017-09-09 11:29:20 +0200

    added fmt command

---
+++ src/commands/fmt.ts
@@ -0,0 +1,16 @@
+import { Command, command, Context, metadata, param } from 'clime'
+import { existsSync, writeFileSync } from 'fs'
+import { join } from 'path'
+import * as shell from 'shelljs'
+import { lint, prettier } from '../lib/scripts'
+
+@command({ description: 'normalize code style and formatting' })
+export default class extends Command {
+  @metadata
+  execute() {
+    if (process.env.NODE_ENV !== 'test') {
+      lint()
+      prettier()
+    }
+  }
+}
