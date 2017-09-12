791c2c687e477fa6a9ec725bc7071ccc101941d7
Maximilian Stroh <Hisako1337@gmail.com>
2017-09-12 21:44:23 +0200

    bump seagull version, include empty provider in frontend template

--- templates/app/frontend/routes.tsx
+++ templates/app/frontend/routes.tsx
@@ -1,6 +1,7 @@
 // library imports
 import { history } from '@seagull-js/seagull'
 import createElement from 'inferno-create-element';
+import { Provider } from 'inferno-mobx'
 import { Route, Router } from 'inferno-router';
 
 // import of individual pages
@@ -8,9 +9,11 @@ import HelloPage from './pages/hello'
 
 // routing structure
 const routes = (
-  <Router history={ history }>
-    <Route path='/' component={ HelloPage }/>
-  </Router>
+  <Provider>
+    <Router history={ history }>
+      <Route path='/' component={ HelloPage }/>
+    </Router>
+  </Provider>
 )
 
 export default routes
