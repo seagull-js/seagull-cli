3c8a74064cea8f1788ceb5d4b74ccebb939fd5f7
Tom Jaster <futur3.tom@googlemail.com>
2017-11-01 22:34:37 +0100

    updated template

--- templates/app/api/Frontend.ts
+++ templates/app/api/Frontend.ts
@@ -1,16 +1,15 @@
 // library imports
-import { API, Request, Response } from '@seagull-js/seagull'
+import { API, Request, Response, Routing } from '@seagull-js/seagull'
 import { renderToString as render } from 'react-dom/server';
 
 // configuration imports
 import layout from '../frontend/layout'
-import routes from '../frontend/routes'
 
 // Server Side Rendering for the frontend
 export default class Frontend extends API {
   static method = 'GET'
   static path = '/*'
   async handle(request: Request): Promise<Response> {
-    return this.html(render(layout({ children: routes(true, request) })))
+    return this.html(render(layout({ children: new Routing(true, request).load() })))
   }
 }
