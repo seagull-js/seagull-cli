ac764bc014564f11bc28bd38486a199110a307d7
Maximilian Stroh <Hisako1337@gmail.com>
2018-02-02 17:29:10 +0100

    local database usage

--- src/commands/dev.ts
+++ src/commands/dev.ts
@@ -12,7 +12,7 @@ export default class extends Command {
     log('> starting dev server with live reload...')
     const nodemon = require('nodemon')
     nodemon({
-      exec: 'seagull build -o false && seagull serve',
+      exec: 'seagull build -o false && NODE_ENV=dev seagull serve',
       ext: 'json,ts,tsx',
       watch: ['frontend', 'backend', 'package.json'],
     })
