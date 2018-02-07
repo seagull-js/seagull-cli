c873cebc9a5bfd8c6a87cbac9671be7caf46e899
Tom Jaster <futur3.tom@googlemail.com>
2018-02-07 21:33:39 +0100

    fixed test

--- src/lib/build/helper.ts
+++ src/lib/build/helper.ts
@@ -2,7 +2,7 @@ import { join } from 'path'
 import * as shell from 'shelljs'
 
 export function binPath(name: string): string {
-  return join(__dirname, '..', '..', 'node_modules', '.bin', name)
+  return join('node_modules', '.bin', name)
 }
 
 export function cleanBuildDirectory() {
