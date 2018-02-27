c7db14f7a688517db5e9f9f2553ae07a012cf324
Peter Melinat <peter.melinat@gecko.de>
2018-02-27 09:22:01 +0100

    upload assets instead of dist

--- src/lib/serverless/s3/sync.ts
+++ src/lib/serverless/s3/sync.ts
@@ -9,6 +9,6 @@ export async function syncS3Data() {
   const accountId = await getAccountId()
   const name = require(`${shell.pwd()}/package.json`).name.replace(/\s/g, '')
   shell.exec(
-    `aws s3 sync --region=${region} --delete ./dist s3://${name}-${accountId}-assets-bucket`
+    `aws s3 sync --region=${region} --delete ./assets s3://${name}-${accountId}-assets-bucket`
   )
 }
