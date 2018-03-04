d011698caf6ec26b603ba223938f77f3d8546bd1
Maximilian Stroh <Hisako1337@gmail.com>
2018-03-04 21:08:51 +0100

    tracking fix

--- src/commands/enable/analytics.ts
+++ src/commands/enable/analytics.ts
@@ -1,5 +1,6 @@
 import { generateAPI } from '@seagull/code-generators'
 import { Command, command, param } from 'clime'
+import { writeFileSync } from 'fs'
 import { join } from 'path'
 import * as shell from 'shelljs'
 import { log } from '../../lib/logger'
@@ -25,5 +26,17 @@ export default class extends Command {
     const dest = join(pwd, 'backend', 'api', `Track.ts`)
     gen.toFile(dest)
     log(`created backend tracking api in: ${dest}`)
+
+    // update package.json
+    const pkgPath = join(pwd, 'package.json')
+    const pkg: any = require(pkgPath)
+    const sgConfig = pkg.seagull || {}
+    sgConfig.analytics = {
+      enabled: true,
+      ga: id,
+    }
+    pkg.seagull = sgConfig
+    writeFileSync(pkgPath, JSON.stringify(pkg, null, 2), 'utf-8')
+    log('updated package.json seagull settings')
   }
 }
