812894b18e348801b6bbeea0a6a99fa62a70ed63
Maximilian Stroh <Hisako1337@gmail.com>
2018-03-06 22:05:43 +0100

    added robots.txt generator

---
+++ src/commands/add/robots.ts
@@ -0,0 +1,17 @@
+import { generateRobotsTxt } from '@seagull/code-generators'
+import { Command, command } from 'clime'
+import { join } from 'path'
+import * as shell from 'shelljs'
+import { log } from '../../lib/logger'
+
+// tslint:disable-next-line:max-classes-per-file
+@command({ description: 'scaffold a robots.txt API handler' })
+export default class extends Command {
+  execute() {
+    const gen = generateRobotsTxt()
+    const pwd = shell.pwd().toString()
+    const dest = join(pwd, 'backend', 'api', `RobotsTxt.ts`)
+    gen.toFile(dest)
+    log(`created RobotsTxt.ts in: ${dest}`)
+  }
+}
